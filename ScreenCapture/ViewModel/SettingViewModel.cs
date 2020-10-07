7b6a55ef26915fb6030ab639d4ff967db4ca4152
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2020-10-07 16:58:24 +0900

    Command 구현

--- ScreenCapture/ViewModel/SettingViewModel.cs
+++ ScreenCapture/ViewModel/SettingViewModel.cs
@@ -5,10 +5,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ScreenCapture.ViewModel
 {
     public class SettingViewModel : ViewModelBase
     {
+        #region Command
+        public RelayCommand<RoutedEventArgs> ButtonClickCommand { get; private set; }
+
+        private void InitRelayCommand()
+        {
+            ButtonClickCommand = new RelayCommand<RoutedEventArgs>(OnButtonClick);
+        }
+        #endregion
+
+        private void OnButtonClick(RoutedEventArgs param)
+        {
+            MessageBox.Show("Hi!");
+        }
+
+        public SettingViewModel()
+        {
+            InitRelayCommand();
+        }
     }
 }

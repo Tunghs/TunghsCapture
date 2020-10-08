b2fda88042bef7e70887be4b7638300006c8d10b
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2020-10-08 16:53:57 +0900

    ItemsControl 추가

---
+++ ScreenCapture/Model/EachClassSettingItem.cs
@@ -0,0 +1,49 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace ScreenCapture.Model
+{
+    public class EachClassSettingItem : ObservableObject
+    {
+        private string _header;
+        public string Header
+        {
+            get => _header;
+            set => Set(ref _header, value);
+        }
+
+        private int _positionX;
+        public int PositionX
+        {
+            get => _positionX;
+            set => Set(ref _positionX, value);
+        }
+
+        private int _positionY;
+        public int PositionY
+        {
+            get => _positionY;
+            set => Set(ref _positionY, value);
+        }
+
+        private int _width;
+        public int Width
+        {
+            get => _width;
+            set => Set(ref _width, value);
+        }
+
+        private int _height;
+        public int Heght
+        {
+            get => _height;
+            set => Set(ref _height, value);
+        }
+
+        private string _savePath;
+        public string SavePath
+        {
+            get => _savePath;
+            set => Set(ref _savePath, value);
+        }
+    }
+}
--- ScreenCapture/ViewModel/SettingViewModel.cs
+++ ScreenCapture/ViewModel/SettingViewModel.cs
@@ -6,23 +6,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using ScreenCapture.Model;
+using System.Collections.ObjectModel;
 
 namespace ScreenCapture.ViewModel
 {
     public class SettingViewModel : ViewModelBase
     {
-        #region Command
-        public RelayCommand<RoutedEventArgs> ButtonClickCommand { get; private set; }
+        #region UI Variable
+        public ObservableCollection<EachClassSettingItem> SGClassCollection { get; set; } = new ObservableCollection<EachClassSettingItem>();
 
-        private void InitRelayCommand()
+        private string _settingName;
+        public string SettingName
         {
-            ButtonClickCommand = new RelayCommand<RoutedEventArgs>(OnButtonClick);
+            get => _settingName;
+            set => Set(ref _settingName, value);
+        }
+
+        private string _settingPath;
+        public string SettingPath
+        {
+            get => _settingPath;
+            set => Set(ref _settingPath, value);
         }
         #endregion
 
-        private void OnButtonClick(RoutedEventArgs param)
+        #region Command
+        public RelayCommand<object> ButtonClickCommand { get; private set; }
+        public RelayCommand<object> CollectionItemButtonClickCommand { get; private set; }
+
+        private void InitRelayCommand()
         {
-            MessageBox.Show("Hi!");
+            ButtonClickCommand = new RelayCommand<object>(OnButtonClick);
+            CollectionItemButtonClickCommand = new RelayCommand<object>(OnCollectionItemButtonClick);
+        }
+        #endregion
+
+        private void OnButtonClick(object param)
+        {
+            switch (param.ToString())
+            {
+                case "Add":
+                    AddSGSettingItem();
+                    break;
+            }
+        }
+
+        private void OnCollectionItemButtonClick(object param)
+        {
+            string header = param.ToString();
+            var eachClassSettingItem = SGClassCollection.Where(x => x.Header == header).ToList()[0];
+            SGClassCollection.Remove(eachClassSettingItem);
+        }
+
+        private void AddSGSettingItem()
+        {
+            SGClassCollection.Add(new EachClassSettingItem() { Header = "Index" + SGClassCollection.Count });
         }
 
         public SettingViewModel()

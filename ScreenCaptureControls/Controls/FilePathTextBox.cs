d3ce100e0fe2df71867e6d370ec3c401a89cde48
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2020-10-15 10:53:31 +0900

    컨트롤 추가

---
+++ ScreenCaptureControls/Controls/FilePathTextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ScreenCaptureControls.Controls
+{
+    public class FilePathTextBox : Control
+    {
+    }
+}

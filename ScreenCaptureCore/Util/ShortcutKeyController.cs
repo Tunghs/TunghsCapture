6d53e97527959926d77794ba2cc44a96c231d549
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2020-10-16 23:00:52 +0900

    업데이트

---
+++ ScreenCaptureCore/Util/ShortcutKeyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ScreenCaptureCore.Util
+{
+    public static class ShortcutKeyController
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, List<Key> keyList);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private static IntPtr
+    }
+}
